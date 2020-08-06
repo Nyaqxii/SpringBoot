ac22a6b16a9789756ffaced4db939fdf6f7b1673
Hasaneeya Kaenthram <hasaneeyakae@mathregit.group>
2020-08-06 09:01:49 +0700

    update ref.

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 //Ref. = https://medium.com/@proSbeginner/spring-boot-pagination-and-sorting-96aadbfc18b0
 //Ref. = https://www.baeldung.com/spring-data-jpa-pagination-sorting
-//Ref. =
+//Ref. = https://github.com/joker10130-dev/springboot/tree/step_05
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
     Page<User> findAll(Pageable pageable);
 }
